a1f57653910cefc4af633f98c0adcd57dff8e03f
Bartek Kryza <bkryza@gmail.com>
2022-06-30 23:35:59 +0200

    Added basic command line options

--- main.cc
+++ main.cc
@@ -20,6 +20,7 @@
 #include <clang-c/CXCompilationDatabase.h>
 #include <boost/graph/adjacency_list.hpp>
 #include <boost/graph/topological_sort.hpp>
+#include <boost/program_options.hpp>
 
 #include <iostream>
 #include <filesystem>
@@ -27,6 +28,11 @@
 #include <vector>
 
 using namespace std;
+namespace po = boost::program_options;
+
+#ifndef LIBCLANG_VERSION_STRING
+#define LIBCLANG_VERSION_STRING "0.0.0"
+#endif
 
 using edges_t = std::set<std::pair<std::string, std::string>>;
 using vertices_t = std::set<std::string>;
@@ -36,98 +42,64 @@ using vertices_names_t = std::map<unsigned int, std::string>;
 using graph_t =
         boost::adjacency_list<boost::setS, boost::vecS, boost::directedS>;
 
-struct include_path_t{
+struct include_path_t {
     using kind = boost::vertex_property_tag;
 };
 
 using graph_property_t = boost::property<include_path_t, std::string>;
 
-ostream &operator<<(ostream &stream, const CXString &str) {
-    stream << clang_getCString(str);
-    clang_disposeString(str);
-    return stream;
-}
+void print_version();
 
-std::string current_file;
+void process_command_line_options(int argc, char **argv, po::variables_map &vm);
+
+void print_diagnostics(const CXTranslationUnit &tu);
 
 void
 inclusion_visitor(CXFile cx_file, CXSourceLocation *inclusion_stack, unsigned include_len,
-                  CXClientData edges_ptr) {
+                  CXClientData edges_ptr);
 
-    auto *edge_set = static_cast<edges_t *>(edges_ptr);
+std::string to_absolute_path(std::string_view relative_path);
 
-    auto cx_file_name = clang_getFileName(cx_file);
-    std::string file_name = clang_getCString(cx_file_name);
-    auto include_path = std::filesystem::canonical(std::filesystem::path(file_name));
-    //cout << "# " << include_path.string() << " : " << include_len << std::endl;
+std::string current_file;
 
-    std::vector<std::string> includes;
-    includes.push_back(include_path.string());
 
-    if (inclusion_stack != nullptr) {
-        for (auto i = 0U; i < include_len; i++) {
-
-            if (i > 0) {
-                clang_getSpellingLocation(inclusion_stack[i - 1],
-                                          &cx_file, nullptr, nullptr, nullptr
-                );
-                cx_file_name = clang_getFileName(cx_file);
-                file_name = clang_getCString(cx_file_name);
-                include_path = std::filesystem::canonical(std::filesystem::path(file_name));
-                //cout << "# " << include_path.string() << " : " << include_len << std::endl;
-            }
-
-            CXFile cx_included_from;
-
-            clang_getSpellingLocation(inclusion_stack[i],
-                                      &cx_included_from, nullptr, nullptr, nullptr
-            );
-
-            if (cx_included_from != nullptr) {
-                auto cx_from_file_name = clang_getFileName(cx_included_from);
-                std::string f = clang_getCString(cx_from_file_name);
-
-                auto from_path = std::filesystem::canonical(std::filesystem::path(f));
-
-                //cout << "### " <<  from_path.string() << std::endl;
-
-                includes.push_back(from_path.string());
-
-                edge_set->insert(std::pair(include_path.string(), from_path.string()));
-            }
-        }
-    }
-}
-
-void print_diagnostics(const CXTranslationUnit &tu) {
-    auto no = clang_getNumDiagnostics(tu);
-    for (auto i = 0u; i != no; ++i) {
-        auto diag = clang_getDiagnostic(tu, i);
-        auto diag_loc = clang_getDiagnosticLocation(diag);
-        CXString diag_file;
-        unsigned line;
-        clang_getPresumedLocation(diag_loc, &diag_file, &line, nullptr);
-        std::string text = clang_getCString(clang_getDiagnosticSpelling(diag));
-
-        cout << "[" << clang_getCString(diag_file) << ":" << line << "] " << text << std::endl;
-    }
-}
-
-int main(int /*argc*/, char **/*argv*/) {
+int main(int argc, char **argv) {
     edges_t edges;
     graph_t include_graph;
     vertices_t vertices;
     vertices_ids_t vertices_ids;
     vertices_names_t vertices_names;
 
+
+    po::variables_map vm;
+    process_command_line_options(argc, argv, vm);
+
+    bool verbose{false};
+    if (vm.count("verbose") == 1)
+        verbose = true;
+
+    std::string compilation_database_directory{to_absolute_path(".")};
+
+    if (vm.count("compilation-database-dir") == 1) {
+        compilation_database_directory = to_absolute_path(vm["compilation-database-dir"].as<std::string>());
+    }
+
+    if (verbose)
+        cout << "=== Loading compilation database from " << compilation_database_directory << std::endl;
+
     CXIndex index = clang_createIndex(0, 0);
 
-    cout << "Loading compilation database";
-
     auto error = CXCompilationDatabase_NoError;
-    auto database = clang_CompilationDatabase_fromDirectory(".", &error);
+    auto database = clang_CompilationDatabase_fromDirectory(compilation_database_directory.c_str(), &error);
 
-    auto compile_commands = clang_CompilationDatabase_getAllCompileCommands(database);
+    CXCompileCommands compile_commands;
+    if (vm.count("translation-unit") == 1) {
+        auto tu_path = to_absolute_path(vm["translation-unit"].as<std::string>());
+        compile_commands = clang_CompilationDatabase_getCompileCommands(database, tu_path.c_str());
+    } else {
+        // Parse entire compilation database
+        compile_commands = clang_CompilationDatabase_getAllCompileCommands(database);
+    }
 
     auto compile_commands_size = clang_CompileCommands_getSize(compile_commands);
 
@@ -137,14 +109,14 @@ int main(int /*argc*/, char **/*argv*/) {
     }
 
     for (auto command_it = 0U; command_it < compile_commands_size; command_it++) {
-
         CXCompileCommand command = clang_CompileCommands_getCommand(compile_commands, command_it);
 
         current_file = clang_getCString(clang_CompileCommand_getFilename(command));
         auto include_path = std::filesystem::canonical(std::filesystem::path(current_file));
         auto include_path_str = include_path.string();
 
-        cout << "=== Processing translation unit: " << include_path_str << std::endl;
+        if (verbose)
+            cout << "=== Processing translation unit: " << include_path_str << std::endl;
 
         std::vector<std::string> args;
         std::vector<const char *> args_cstr;
@@ -154,7 +126,6 @@ int main(int /*argc*/, char **/*argv*/) {
             args.push_back(clang_getCString(clang_CompileCommand_getArg(command, i)));
             args_cstr.push_back(args[i].c_str());
         }
-
 
         // Remove the file name from the arguments list
         args.pop_back();
@@ -189,7 +160,7 @@ int main(int /*argc*/, char **/*argv*/) {
     }
 
     unsigned vertice_id = 0;
-    for(const auto& v : vertices) {
+    for (const auto &v: vertices) {
         vertices_ids[v] = vertice_id;
         vertices_names[vertice_id] = v;
         vertice_id++;
@@ -202,9 +173,108 @@ int main(int /*argc*/, char **/*argv*/) {
     std::deque<unsigned int> include_order;
     boost::topological_sort(include_graph, std::front_inserter(include_order));
 
-    for(const auto id : include_order) {
-        cout << id << " : " << vertices_names[id] << endl;
+    for (const auto id: include_order) {
+        cout << vertices_names[id] << endl;
     }
 
     clang_disposeIndex(index);
+}
+
+void process_command_line_options(int argc, char **argv, po::variables_map &vm) {
+    po::options_description options("clang-include-graph options");
+    options.add_options()
+            ("help,h", "Print help message and exit")
+            ("version,V", "Print program version and exit")
+            ("verbose,v", "Print verbose information during processing")
+            ("compilation-database-dir,d", po::value<std::string>(),
+             "Path to compilation database directory (default $PWD)")
+            ("translation-unit,u", po::value<std::string>(), "Process a single source file from compilation database")
+            ("tree,t", "Print output graph in tree form");
+
+
+    po::store(po::parse_command_line(argc, argv, options), vm);
+    po::notify(vm);
+
+
+    if (vm.count("help")) {
+        cout << options << "\n";
+        exit(0);
+    }
+
+    if (vm.count("version")) {
+        print_version();
+        exit(0);
+    }
+}
+
+void print_version() {
+    std::cout << "clang-include-graph 0.1.0" << std::endl;
+    std::cout << "Copyright (C) 2022 Bartek Kryza <bkryza@gmail.com>"
+              << '\n';
+    std::cout << "Built with libclang: "
+              << LIBCLANG_VERSION_STRING << std::endl;
+}
+
+
+void print_diagnostics(const CXTranslationUnit &tu) {
+    auto no = clang_getNumDiagnostics(tu);
+    for (auto i = 0u; i != no; ++i) {
+        auto diag = clang_getDiagnostic(tu, i);
+        auto diag_loc = clang_getDiagnosticLocation(diag);
+        CXString diag_file;
+        unsigned line;
+        clang_getPresumedLocation(diag_loc, &diag_file, &line, nullptr);
+        std::string text = clang_getCString(clang_getDiagnosticSpelling(diag));
+
+        cout << "[" << clang_getCString(diag_file) << ":" << line << "] " << text << std::endl;
+    }
+}
+
+void
+inclusion_visitor(CXFile cx_file, CXSourceLocation *inclusion_stack, unsigned include_len,
+                  CXClientData edges_ptr) {
+
+    auto *edge_set = static_cast<edges_t *>(edges_ptr);
+
+    auto cx_file_name = clang_getFileName(cx_file);
+    std::string file_name = clang_getCString(cx_file_name);
+    auto include_path = std::filesystem::canonical(std::filesystem::path(file_name));
+
+    std::vector<std::string> includes;
+    includes.push_back(include_path.string());
+
+    if (inclusion_stack != nullptr) {
+        for (auto i = 0U; i < include_len; i++) {
+
+            if (i > 0) {
+                clang_getSpellingLocation(inclusion_stack[i - 1],
+                                          &cx_file, nullptr, nullptr, nullptr
+                );
+                cx_file_name = clang_getFileName(cx_file);
+                file_name = clang_getCString(cx_file_name);
+                include_path = std::filesystem::canonical(std::filesystem::path(file_name));
+            }
+
+            CXFile cx_included_from;
+
+            clang_getSpellingLocation(inclusion_stack[i],
+                                      &cx_included_from, nullptr, nullptr, nullptr
+            );
+
+            if (cx_included_from != nullptr) {
+                auto cx_from_file_name = clang_getFileName(cx_included_from);
+                std::string f = clang_getCString(cx_from_file_name);
+
+                auto from_path = std::filesystem::canonical(std::filesystem::path(f));
+
+                includes.push_back(from_path.string());
+
+                edge_set->insert(std::pair(include_path.string(), from_path.string()));
+            }
+        }
+    }
+}
+
+std::string to_absolute_path(std::string_view relative_path) {
+    return std::filesystem::canonical(std::filesystem::path(relative_path)).string();
 }
