6aff3ae2e6685aa037316bd0142807b0d186e45e
Bartek Kryza <bkryza@gmail.com>
2022-07-09 13:08:30 +0200

    Added graphviz printer testcase

--- tests/test_graphviz_printer.cc
+++ tests/test_graphviz_printer.cc
@@ -1,5 +1,5 @@
 /**
-* tests/test_plantuml_printer.cc
+* tests/test_graphviz_printer.cc
 *
 * Copyright (c) 2022-present Bartek Kryza <bkryza@gmail.com>
 *
@@ -14,42 +14,42 @@
 * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 * See the License for the specific language governing permissions and
 * limitations under the License.
-*/
+ */
 
-#define BOOST_TEST_MODULE Unit test of include_graph_plantuml_printer_t
+#define BOOST_TEST_MODULE Unit test of include_graph_graphviz_printer_t
 
 #include <boost/test/unit_test.hpp>
 
-#include "../src/include_graph_plantuml_printer.h"
+#include "../src/include_graph_graphviz_printer.h"
 
 #include <sstream>
 
 using namespace clang_include_graph;
 
-BOOST_AUTO_TEST_CASE(test_simple_plantuml)
+BOOST_AUTO_TEST_CASE(test_simple_graphviz)
 {
-   include_graph_t graph;
-   graph.add_edge("include1.h", "main.cc", true);
-   graph.add_edge("include2.h", "main.cc", true);
-   graph.add_edge("include3.h", "include1.h");
+    include_graph_t graph;
+    graph.add_edge("include1.h", "main.cc", true);
+    graph.add_edge("include2.h", "main.cc", true);
+    graph.add_edge("include3.h", "include1.h");
 
-   path_printer_t pp;
+    path_printer_t pp;
 
-   include_graph_plantuml_printer_t p{graph, pp};
+    include_graph_graphviz_printer_t p{graph, pp};
 
-   std::stringstream ss;
-   ss << p;
+    std::stringstream ss;
+    ss << p;
 
-   std::string expected = R"(@startuml
-file "include1.h" as F_0
-file "main.cc" as F_1
-file "include2.h" as F_2
-file "include3.h" as F_3
-F_0 -->  F_3
-F_1 -->  F_0
-F_1 -->  F_2
-@enduml
+    std::string expected = R"(digraph G {
+0[label="include1.h"];
+1[label="main.cc"];
+2[label="include2.h"];
+3[label="include3.h"];
+0->3 ;
+1->0 ;
+1->2 ;
+}
 )";
 
-   BOOST_TEST(ss.str() == expected);
+    BOOST_TEST(ss.str() == expected);
 }
