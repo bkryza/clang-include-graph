f16c72a61174400bff98c73f890fad240ac50b10
Bartek Kryza <bkryza@gmail.com>
2025-05-14 15:44:27 +0200

    Added Windows packaging files

---
+++ packaging/make_installer.ps1
@@ -0,0 +1,18 @@
+# This script assumes that all clang-uml dependencies are instaled in C:\clang-uml
+
+param ($Prefix="C:\clang-uml-llvm19", $BuildType="Release")
+
+mkdir _BUILD
+
+cmake -S .. -B .\_BUILD\windows\ -DCMAKE_PREFIX_PATH="$Prefix" -Thost=x64
+
+cd .\_BUILD\windows
+
+msbuild .\clang-include-graph.vcxproj -maxcpucount /p:Configuration=Release
+
+cd ..
+
+cpack -C "Release" -G NSIS64 
+
+cd ..
+cd ..
